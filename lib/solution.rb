6397d5f4f17b6e1ab0081cc46a89c1b1024ef186
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-26 10:03:45 -0800

    commit tree 4f9d9ee683da787b3fc470c76bc1953cdd4b2427

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    hours = time.split(":")[0].to_f
+    minutes = time.split(":")[1].to_f
+    minutes_angle = (60.0/360.0) * minutes
+    hours_angle = (12.0/360.0) * hours
+    minutes_angle + hours_angle
+    binding.pry
+
+
 end
